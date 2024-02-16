05767bf38fbf1b8f5a3a7e3b92b44122c2c5d627
ewestenburg <ashtonwestenburg@gmail.com>
2024-02-15 21:16:31 -0800

    Lambda function

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -2,48 +2,62 @@
 #define AS1CPP
 
 #include "raylib-cpp.hpp"
+#include "raymath.h"
+
+// Function to translate models using a lambda before drawing
+void TranslateAndDraw(raylib::Model& model, const raylib::Vector3& position, const raylib::Vector3& scale, const raylib::Vector3& axis, const float& angle, const raylib::Color& color) {
+    model.transform = raylib::Transform(model.transform).Translate(position);
+    model.transform = raylib::Transform(model.transform).Scale(scale.x, scale.y, scale.z);
+    model.transform = raylib::Transform(model.transform).Rotate(axis, angle);
+    DrawModel(model, position, 0.01f, color);
+
+    // Draw bounding box
+    //DrawBoundingBox(model.mesh.bounds, position, 1.0f, color);
+}
 
 int main() {
+    
     raylib::Window window(800, 600, "CS381 - Assignment 1");
 
     raylib::Model plane = LoadModel("/home/ashton/cs381/as1/381Resources/meshes/PolyPlane.glb");
     plane.transform = raylib::Transform(plane.transform).Scale(3, 3, 3);
 
-    raylib::Model ship = LoadModel("/home/ashton/cs381/as1/381Resources/meshes/OilTanker.glb");
+    raylib::Model ship = LoadModel("/home/ashton/cs381/as1/381Resources/meshes/ddg51.glb");
     ship.transform = raylib::Transform(ship.transform).RotateXYZ(raylib::Degree(90), 0, raylib::Degree(90));
 
     raylib::Camera3D camera;
     camera.target = raylib::Vector3(0, 0, 0);
-    camera.position = raylib::Vector3(100, 100, 100);
-    camera.up = raylib::Vector3::Up();
+    camera.position = raylib::Vector3(0, 120, -500);
+    camera.up = (raylib::Vector3){0.0f, 1.0f, 0.0f};
     camera.fovy = 45;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    SetTargetFPS(60);
     DisableCursor();
+    SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())       
-    {
+    while (!WindowShouldClose()) {
         UpdateCamera(&camera, CAMERA_FREE);
 
-        if (IsKeyPressed('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+        if (IsKeyPressed('Z')) camera.target = (raylib::Vector3){0.0f, 0.0f, 0.0f};
 
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            BeginMode3D(camera);
+        BeginMode3D(camera);
 
-                plane.Draw({0, 1, 0}, 1.0f, {255, 255, 255, 255});
+        // Draw airplanes and ships with different positions, scales, and rotations
+        TranslateAndDraw(plane, {0, 0, 0}, {1, 1, 1}, {0, 0, 0}, 0, {255, 255, 255, 255});  // Default parameters
+        TranslateAndDraw(plane, {-100, 100, 0}, {1, -1, 1}, {0, 1, 0}, raylib::Degree(180), {255, 255, 255, 255});
+        TranslateAndDraw(ship, {-200, 0, 0}, {1, 1, 1}, {0, 0, 0}, 0, {255, 255, 255, 255});  // Default parameters
+        TranslateAndDraw(ship, {200, 0, 0}, {1, 1, 1}, {0, 1, 0}, raylib::Degree(90), {255, 255, 255, 255});
+        TranslateAndDraw(ship, {100, 100, 0}, {1, 2, 1}, {0, 1, 0}, raylib::Degree(270), {255, 255, 255, 255});
 
-            EndMode3D();
+        DrawGrid(100, 1.0f);
 
-            DrawRectangle( 10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 320, 93, BLUE);
+        EndMode3D();
 
         EndDrawing();
-
     }
     CloseWindow();
 }

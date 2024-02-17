cf718cf09b26a247d31219411a3cbcf3a2b169f5
ewestenburg <ashtonwestenburg@gmail.com>
2024-02-16 16:09:14 -0800

    Bounding Box

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -3,16 +3,17 @@
 
 #include "raylib-cpp.hpp"
 #include "raymath.h"
+#include <thread>
 
 // Function to translate models using a lambda before drawing
 void TranslateAndDraw(raylib::Model& model, const raylib::Vector3& position, const raylib::Vector3& scale, const raylib::Vector3& axis, const float& angle, const raylib::Color& color) {
     model.transform = raylib::Transform(model.transform).Translate(position);
     model.transform = raylib::Transform(model.transform).Scale(scale.x, scale.y, scale.z);
     model.transform = raylib::Transform(model.transform).Rotate(axis, angle);
-    DrawModel(model, position, 0.01f, color);
+    DrawModel(model, position, 1.0f, color);
 
-    // Draw bounding box
-    //DrawBoundingBox(model.mesh.bounds, position, 1.0f, color);
+    BoundingBox box = model.GetBoundingBox();
+    DrawBoundingBox(box, BLACK);
 }
 
 int main() {
@@ -22,7 +23,7 @@ int main() {
     raylib::Model plane = LoadModel("/home/ashton/cs381/as1/381Resources/meshes/PolyPlane.glb");
     plane.transform = raylib::Transform(plane.transform).Scale(3, 3, 3);
 
-    raylib::Model ship = LoadModel("/home/ashton/cs381/as1/381Resources/meshes/ddg51.glb");
+    raylib::Model ship = LoadModel("/home/ashton/cs381/as1/381Resources/meshes/SmitHouston_Tug.glb");
     ship.transform = raylib::Transform(ship.transform).RotateXYZ(raylib::Degree(90), 0, raylib::Degree(90));
 
     raylib::Camera3D camera;
@@ -48,12 +49,14 @@ int main() {
 
         // Draw airplanes and ships with different positions, scales, and rotations
         TranslateAndDraw(plane, {0, 0, 0}, {1, 1, 1}, {0, 0, 0}, 0, {255, 255, 255, 255});  // Default parameters
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         TranslateAndDraw(plane, {-100, 100, 0}, {1, -1, 1}, {0, 1, 0}, raylib::Degree(180), {255, 255, 255, 255});
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         TranslateAndDraw(ship, {-200, 0, 0}, {1, 1, 1}, {0, 0, 0}, 0, {255, 255, 255, 255});  // Default parameters
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         TranslateAndDraw(ship, {200, 0, 0}, {1, 1, 1}, {0, 1, 0}, raylib::Degree(90), {255, 255, 255, 255});
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         TranslateAndDraw(ship, {100, 100, 0}, {1, 2, 1}, {0, 1, 0}, raylib::Degree(270), {255, 255, 255, 255});
-
-        DrawGrid(100, 1.0f);
 
         EndMode3D();
 

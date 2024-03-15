f4df371c01d00010838a3391b27c85d6f7bcdfa6
ewestenburg <ashtonwestenburg@gmail.com>
2024-03-14 20:30:18 -0700

    plays music

--- as5/src/as5.cpp
+++ as5/src/as5.cpp
@@ -4,38 +4,36 @@
 #include "VolumeControl.h"
 
 GuiVolumeControlState volumeControlState;
+raylib::AudioDevice audio;    
 
 Sound soundEffect;
-Sound dialogueTrack;
+Music dialogueTrack;
 Music backgroundMusic;
 
 bool buttonPressed = false;
 
 void LoadAudioFiles() {
     soundEffect = LoadSound("381Resources/audio/ping.wav");
-    dialogueTrack = LoadSound("381Resources/audio/crowd.wav");
-    backgroundMusic = LoadMusicStream("381Resources/audio/price-of-freedom.wav");
-}
-
-void PlayAudioFiles() {
-    PlaySound(soundEffect);
-    PlaySound(dialogueTrack);
+    dialogueTrack = LoadMusicStream("381Resources/audio/crowd.wav");
+    backgroundMusic = LoadMusicStream("381Resources/audio/price-of-freedom.mp3");
     PlayMusicStream(backgroundMusic);
     SetMusicVolume(backgroundMusic, volumeControlState.MusicSliderValue / 100.0f);
-    SetSoundVolume(soundEffect, volumeControlState.SFXSliderValue / 100.0f);
-    SetSoundVolume(dialogueTrack, volumeControlState.DialogueSliderValue / 100.0f);
+    PlayMusicStream(dialogueTrack);
+    SetMusicVolume(dialogueTrack, volumeControlState.MusicSliderValue / 100.0f);
 }
 
 void UpdateAudio() {
     UpdateMusicStream(backgroundMusic);
+    SetMusicVolume(backgroundMusic, volumeControlState.MusicSliderValue / 100.0f);
+    SetSoundVolume(soundEffect, volumeControlState.SFXSliderValue / 100.0f);
+    SetMusicVolume(dialogueTrack, volumeControlState.DialogueSliderValue / 100.0f);
 
-    // Check if the button is pressed
-    /*if (GuiButton((Rectangle){24, 304, 256, 24}, "Ping") && !buttonPressed) {
+    if (GuiButton((Rectangle){24, 304, 256, 24}, "Ping") && !buttonPressed) {
         buttonPressed = true;
         PlaySound(soundEffect);
     } else {
         buttonPressed = false;
-    }*/
+    }
 
     // Check if the space bar is pressed
     if (IsKeyPressed(KEY_SPACE)) {
@@ -64,7 +62,7 @@ int main() {
 
     // cleanup
     UnloadSound(soundEffect);
-    UnloadSound(dialogueTrack);
+    UnloadMusicStream(dialogueTrack);
     UnloadMusicStream(backgroundMusic);
     CloseWindow();
 

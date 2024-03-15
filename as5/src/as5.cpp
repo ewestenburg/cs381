35a62de9ee6c3d55292542ec64a8a315c85c5d04
ewestenburg <ashtonwestenburg@gmail.com>
2024-03-14 20:35:42 -0700

    plays crowd audio

--- as5/src/as5.cpp
+++ as5/src/as5.cpp
@@ -7,26 +7,26 @@ GuiVolumeControlState volumeControlState;
 raylib::AudioDevice audio;    
 
 Sound soundEffect;
-Music dialogueTrack;
+Sound dialogueTrack;
 Music backgroundMusic;
 
 bool buttonPressed = false;
 
 void LoadAudioFiles() {
     soundEffect = LoadSound("381Resources/audio/ping.wav");
-    dialogueTrack = LoadMusicStream("381Resources/audio/crowd.wav");
+    dialogueTrack = LoadSound("381Resources/audio/crowd.wav");
     backgroundMusic = LoadMusicStream("381Resources/audio/price-of-freedom.mp3");
     PlayMusicStream(backgroundMusic);
     SetMusicVolume(backgroundMusic, volumeControlState.MusicSliderValue / 100.0f);
-    PlayMusicStream(dialogueTrack);
-    SetMusicVolume(dialogueTrack, volumeControlState.MusicSliderValue / 100.0f);
+    PlaySound(dialogueTrack);
+    SetSoundVolume(dialogueTrack, volumeControlState.MusicSliderValue / 100.0f);
 }
 
 void UpdateAudio() {
     UpdateMusicStream(backgroundMusic);
     SetMusicVolume(backgroundMusic, volumeControlState.MusicSliderValue / 100.0f);
     SetSoundVolume(soundEffect, volumeControlState.SFXSliderValue / 100.0f);
-    SetMusicVolume(dialogueTrack, volumeControlState.DialogueSliderValue / 100.0f);
+    SetSoundVolume(dialogueTrack, volumeControlState.DialogueSliderValue / 100.0f);
 
     if (GuiButton((Rectangle){24, 304, 256, 24}, "Ping") && !buttonPressed) {
         buttonPressed = true;
@@ -62,7 +62,7 @@ int main() {
 
     // cleanup
     UnloadSound(soundEffect);
-    UnloadMusicStream(dialogueTrack);
+    UnloadSound(dialogueTrack);
     UnloadMusicStream(backgroundMusic);
     CloseWindow();
 
